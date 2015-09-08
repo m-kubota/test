246a4923acc7aac7514a4a775ebeae11d7d52aea
m-kubota <m-kubota@iid.co.jp>
2015-09-08 12:42:18 +0900

    初期設定

---
+++ target/generated-sources/groovy-stubs/main/page/InsideHomePage.java
@@ -0,0 +1,23 @@
+package page;
+
+import java.lang.*;
+import java.io.*;
+import java.net.*;
+import java.util.*;
+import groovy.lang.*;
+import groovy.util.*;
+
+public class InsideHomePage
+  extends java.lang.Object  implements
+    groovy.lang.GroovyObject {
+;
+public  groovy.lang.MetaClass getMetaClass() { return (groovy.lang.MetaClass)null;}
+public  void setMetaClass(groovy.lang.MetaClass mc) { }
+public  java.lang.Object invokeMethod(java.lang.String method, java.lang.Object arguments) { return null;}
+public  java.lang.Object getProperty(java.lang.String property) { return null;}
+public  void setProperty(java.lang.String property, java.lang.Object value) { }
+public static  java.lang.Object getUrl() { return null;}
+public static  void setUrl(java.lang.Object value) { }
+public static  java.lang.Object getAt() { return null;}
+public static  void setAt(java.lang.Object value) { }
+}
---
+++ target/generated-sources/groovy-stubs/test/InsideMainTest.java
@@ -0,0 +1,11 @@
+import java.lang.*;
+import java.io.*;
+import java.net.*;
+import java.util.*;
+import groovy.lang.*;
+import groovy.util.*;
+
+public class InsideMainTest
+  extends geb.spock.GebReportingSpec {
+;
+}
